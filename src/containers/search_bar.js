77279a3f3fe1c95f79cf5334c52e6cf127f1fc01
Pete DeGraw <petedegraw@gmail.com>
2016-08-25 23:58:39 -0700

    prevent default form submission beviour

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -15,9 +15,15 @@ export default class SearchBar extends Component {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // fetch the weather data
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a 5 day forcast in your favorite cities"
           className="form-control"
