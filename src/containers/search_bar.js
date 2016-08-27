a913a0dd84605163a49e1987ebd4d8af2c90ee27
Pete DeGraw <petedegraw@gmail.com>
2016-08-26 17:43:20 -0700

    adds actions and reducers

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+export class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
 
-    this.onInputChange = this.onInputChange.bind(this);
     // if you want to reference this, you need to define it
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -17,8 +21,9 @@ export default class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
-
-    // fetch the weather data
+    // stop the form from the traditional submit and fetch the weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
@@ -36,3 +41,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
