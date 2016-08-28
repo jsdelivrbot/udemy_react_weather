7442d25d020df8f2c9861ca20e68ab354b552ca0
Pete DeGraw <petedegraw@gmail.com>
2016-08-28 13:03:38 -0700

    adds charts

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,10 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return state.push(action.payload.data);
+      // not mutating state, just adding a new instance of state
+      // return state.concat([action.payload.data]);
+      return [ action.payload.data, ...state ]; // es6 version [ city, city, city ]
+      // new record is entered at the top of the array
   }
   return state;
 }
